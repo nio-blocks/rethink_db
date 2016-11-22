e2aa149627d47ac0756b4c0ce276bfb1d2f80bbf
Dylan Orzel <dylan.orzel@gmail.com>
2016-11-22 16:27:35 -0700

    tweak base block and some functionality for update block

--- rethinkdb_base.py
+++ rethinkdb_base.py
@@ -9,6 +9,8 @@ from nio.util.discovery import not_discoverable, discoverable
 
 @command("Get server info", method="_get_server_info")
 @command("Reconnect to server", method="_reconnect_connection")
+@command("List of databases on server", method="_get_list_of_databases")
+@command("List of tables on current database", method="_get_list_of_tables")
 @not_discoverable
 class RethinkDBBase(Block):
 
@@ -32,25 +34,30 @@ class RethinkDBBase(Block):
 
     def __init__(self):
         super().__init__()
+        # current connection to server
         self._connection = None
+        # current database
+        self._db = None
+        # list of databases on current server
+        self._database_name_list = None
+        # list of tables on the database
+        self._table_name_list = None
 
     def configure(self, context):
         super().configure(context)
         self._connect_to_db()
+        self._get_list_of_databases()
+        self._get_list_of_tables()
 
     def stop(self):
         self.logger.info('closing RethinkDB connection...')
         self._close_connection()
         super().stop()
 
-    def process_signals(self, signals):
-        for signal in signals:
-            self.logger.info('Processing signal: {}'.format(signal))
-        self.notify_signals(signals)
-
     def _connect_to_db(self):
         """form a connection to a database, storing the connection for use
-        in queries, etc. later on
+        in queries, etc. later on. Also form the database that this connection
+        will use.
         """
         self.logger.debug('Connecting to DB...')
         self._connection = rdb.connect(host=self.host(),
@@ -58,6 +65,7 @@ class RethinkDBBase(Block):
                                        db=self.database_name(),
                                        timeout=self.connect_timeout().total_seconds()
                                        )
+        self._db = rdb.db(self.database_name())
 
     def _reconnect_connection(self):
         """close and attempt to reconnect to the existing connection"""
@@ -73,12 +81,57 @@ class RethinkDBBase(Block):
         """returns info about the current connected server"""
         return self._connection.server()
 
+    def _get_list_of_databases(self):
+        """returns a list of database names on the current server and sets it
+        to self._database_name_list
+        """
+        self._database_name_list = rdb.db_list().run(self._connection)
+        return self._database_name_list
+
+    def _get_list_of_tables(self):
+        """returns a list of tables for the current database and sets it to
+        self._table_name_list
+        """
+        self._table_name_list = self._db.table_list().run(self._connection)
+        return self._table_name_list
+
 
 @discoverable
+@command("List of indexes on current table", method="_get_list_of_indexes")
 class RethinkDBUpdate(RethinkDBBase):
 
     table = StringProperty(title="Table to update", default='test')
 
+    def __init__(self):
+        super().__init__()
+        # current table being updated
+        self._table = None
+        # list of all secondary indexes on the table
+        self._list_of_indexes = None
+
+    def configure(self, context):
+        super().configure(context)
+        self._set_table()
+        self._get_list_of_indexes()
+
+    def process_signals(self, signals):
+        for signal in signals:
+            self.logger.info('Processing signal: {}'.format(signal))
+        self.notify_signals(signals)
+
+    def update_table(self):
+        self._table.update()
+
+    def _set_table(self):
+        """set _table to a table object tied to the current db"""
+        self._table = self._db.table(self.table())
+
+    def _get_list_of_indexes(self):
+        """gets the list of all secondary indexes on the current table and
+        sets it to self._list_of_indexes
+        """
+        self._list_of_indexes = self._table.index_list().run(self._connection)
+        return self._list_of_indexes
 
 
 class RethinkDBChanges(RethinkDBBase):
