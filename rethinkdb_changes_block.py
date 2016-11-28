5b6cb87bd1e83d0c09b0ab1041dcf7c2f91ebcef
Dylan Orzel <dylan.orzel@gmail.com>
2016-11-28 11:01:39 -0700

    first draft on filtering by key

--- rethinkdb_changes_block.py
+++ rethinkdb_changes_block.py
@@ -2,4 +2,7 @@ from .rethinkdb_base_block import RethinkDBBase
 
 
 class RethinkDBChanges(RethinkDBBase):
+    """block to watch for changes from a RethinkDB and pass along any specified
+    attributes
+    """
     pass
--- rethinkdb_update_block.py
+++ rethinkdb_update_block.py
@@ -1,49 +1,59 @@
-from nio.properties import StringProperty
+from nio.properties import (StringProperty, ListProperty, PropertyHolder,
+                            Property)
 from nio.util.discovery import discoverable
-from nio.command import command
 from .rethinkdb_base_block import RethinkDBBase
 
 
+class MatchItem(PropertyHolder):
+    key = Property(title='Match key')
+
+
 @discoverable
-@command("List of indexes on current table", method="_get_list_of_indexes")
 class RethinkDBUpdate(RethinkDBBase):
 
+    """a block for updating info in a RethinkDB table"""
+
     table = StringProperty(title="Table to update", default='test')
-    index_to_update = StringProperty(title="Table index to update",
-                                     default='test')
+    filters = ListProperty(MatchItem, title='Match the following document keys',
+                           default=[])
 
     def __init__(self):
         super().__init__()
         # current table being updated
         self._table = None
-        # list of all secondary indexes on the table
-        self._list_of_indexes = None
 
     def configure(self, context):
         super().configure(context)
         self._set_table()
-        self._get_list_of_indexes()
 
     def process_signals(self, signals):
         for signal in signals:
-            self.logger.info('Processing signal: {}'.format(signal))
+            self.logger.debug('Processing signal: {}'.format(signal))
             self.update_table(signal)
         self.notify_signals(signals)
 
     def update_table(self, signal):
-        data = {
-            self.index_to_update(): signal.to_dict()[self.index_to_update()]
-        }
+        """filter by given fields and update the correct document in the
+        table
+        """
+        data = signal.to_dict()
+        filter_dict = {}
+        filters = (filter.key() for filter in self.filters())
 
-        self._table.update(data).run(self._connection)
+        for key in filters:
+            filter_dict.update({key: data[key]})
+
+        self.logger.debug("Update using filters: {}".format(filter_dict))
+
+        field_filter = self._table.filter(filter_dict)
+        result = field_filter.update(data).run(self._connection)
+
+        self.logger.debug("Sent update request, result: {}".format(result))
+        if not sum(result.values()):
+            self.logger.debug('(no document fields matching given filters)')
+
+        self.notify_signals(result)
 
     def _set_table(self):
         """set _table to a table object tied to the current db"""
         self._table = self._db.table(self.table())
-
-    def _get_list_of_indexes(self):
-        """gets the list of all secondary indexes on the current table and
-        sets it to self._list_of_indexes
-        """
-        self._list_of_indexes = self._table.index_list().run(self._connection)
-        return self._list_of_indexes
