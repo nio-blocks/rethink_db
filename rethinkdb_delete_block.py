b0f709c4f593acb30ccf83a14159f31eab3020ca
Dylan Orzel <dylan.orzel@gmail.com>
2017-03-30 10:02:14 -0600

    progress on delete block and test

--- rethinkdb_delete_block.py
+++ rethinkdb_delete_block.py
@@ -2,13 +2,13 @@ import rethinkdb as rdb
 from nio.properties import StringProperty, Property
 from nio.util.discovery import discoverable
 from .rethinkdb_base_block import RethinkDBBase
+from nio.signal.base import Signal
 
 
 @discoverable
 class RethinkDBDelete(RethinkDBBase):
 
-    """a block for querying a rethinkdb table and adding results onto
-    incoming signals"""
+    """a block for deleting one or more documents from a rethinkdb table"""
 
     table = StringProperty(title="Table to query", default='test',
                            allow_none=False)
@@ -19,32 +19,27 @@ class RethinkDBDelete(RethinkDBBase):
     def _locked_process_signals(self, signals):
         notify_list = []
         for signal in signals:
-            self.logger.debug('Filter is Processing signal: {}'.format(signal))
+            self.logger.debug('Delete is Processing signal: {}'.format(signal))
             # update incoming signals with results of the query
-            results = self.execute_with_retry(self._filter, signal)
-            if results:
-                for result in results:
-                    out_sig = self.get_output_signal(result, signal)
-                    notify_list.append(out_sig)
-            else:
-                # always notify a signal. still append empty results to
-                # enriched signals
-                out_sig = self.get_output_signal(results, signal)
-                notify_list.append(out_sig)
-
+            delete_results = self.execute_with_retry(self._delete, signal)
+            self.logger.debug("Delete results: {}".format(delete_results))
+            notify_list.append(Signal(delete_results))
         self.notify_signals(notify_list)
 
-    def _filter(self, signal):
+    def _delete(self, signal):
         with rdb.connect(
                 host=self.host(),
                 port=self.port(),
                 db=self.database_name(),
                 timeout=self.connect_timeout().total_seconds()) as conn:
-            cursor = rdb.db(self.database_name()).table(self.table()).\
-                filter(self.filter(signal)).run(conn)
-            results = list(cursor)
-            cursor.close()
-        self.logger.debug(
-            "Querying using filter {} return results {}".format(
-                self.filter(signal), results))
+
+            # this will return the typical deleted, errors, unchanges, etc.
+            # as well as changes. If the delete was successful, 'new_val' will
+            # be none in changes.
+            results = rdb.db(self.database_name()).table(self.table()).\
+                filter(self.filter(signal)).delete(return_changes=True).\
+                run(conn)
+
+        self.logger.debug("Deleting using filter {} return results: {}"
+                          .format(self.filter(signal), results))
         return results
